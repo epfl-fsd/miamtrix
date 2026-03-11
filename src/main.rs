24755c214072847f7b62e688f80a5c29476ec9e9
antoine <antoine.fabre@epfl.ch>
2026-03-11 15:37:41 +0100

    [feature] Add basic connection to matrix server with trigger message

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use dotenv::dotenv;
+use matrix_sdk::{
+    Client,
+    config::SyncSettings,
+    ruma::events::room::{
+        message::OriginalSyncRoomMessageEvent,
+    },
+};
+mod config;
+
+use crate::config::{AppConfig, CONFIG};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let config_app = AppConfig::load_env();
+
+    let url = config_app.url_server_matrix.clone();
+    let user = config_app.bot_username.clone();
+    let pass = config_app.bot_password.clone();
+    CONFIG.set(config_app).expect("Config already init");
+
+    login_and_sync(url, &user, &pass).await?;
+
+    Ok(())
+}
+
+async fn trigger_message(ev: OriginalSyncRoomMessageEvent) {
+    println!("Nouveau message reçu : {:?}", ev.content.body());
+}
+
+async fn login_and_sync(
+    homeserver_url: String,
+    username: &str,
+    password: &str,
+) -> anyhow::Result<()> {
+    let client = Client::builder()
+        .homeserver_url(homeserver_url)
+        .build()
+        .await?;
+
+    client
+        .matrix_auth()
+        .login_username(username, password)
+        .initial_device_display_name("getting started bot")
+        .await?;
+
+    println!("logged in as {username}");
+    client.add_event_handler(trigger_message);
+    let sync_token = client.sync_once(SyncSettings::default()).await.unwrap().next_batch;
+
+    let settings = SyncSettings::default().token(sync_token);
+    client.sync(settings).await?;
+
+    Ok(())
+}
