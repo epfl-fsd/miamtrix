1a27f6eaae1e9a6136c1f65e28843f66bd5c512d
antoine <antoine.fabre@epfl.ch>
2026-03-17 11:22:03 +0100

    [feature] Add AutoJoin function

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ use dotenv::dotenv;
 use matrix_sdk::{
     Client,
     config::SyncSettings,
+    Room,
     ruma::events::room::{
-        message::OriginalSyncRoomMessageEvent,
+        member::StrippedRoomMemberEvent,
     },
 };
+use tokio::time::{Duration, sleep};
+
 mod config;
 mod services;
 mod utils;
@@ -54,10 +57,41 @@ async fn login_and_sync(
 
     println!("logged in as {username}");
     client.add_event_handler(controller_command);
+    //Add auto join function to handler
+    client.add_event_handler(auto_accept_invites);
     let sync_token = client.sync_once(SyncSettings::default()).await.unwrap().next_batch;
 
     let settings = SyncSettings::default().token(sync_token);
     client.sync(settings).await?;
 
     Ok(())
+}
+
+//Auto join function to accepte invitation
+async fn auto_accept_invites(
+    room_member: StrippedRoomMemberEvent,
+    client: Client,
+    room: Room,
+) {
+    if room_member.state_key != client.user_id().unwrap() {
+            return;
+        }
+
+    tokio::spawn(async move {
+        println!("Autojoining room {}", room.room_id());
+        let mut delay = 2;
+
+        while let Err(err) = room.join().await {
+            eprintln!("Failed to join room {} ({err:?}), retrying in {delay}s", room.room_id());
+
+            sleep(Duration::from_secs(delay)).await;
+            delay *= 2;
+
+            if delay > 3600 {
+                eprintln!("Can't join room {} ({err:?})", room.room_id());
+                break;
+            }
+        }
+        println!("Successfully joined room {}", room.room_id());
+    });
 }
