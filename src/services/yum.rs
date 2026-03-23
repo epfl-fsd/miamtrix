b101995c6566f7babc9d3acd676fcfd39c603584
antoine <antoine.fabre@epfl.ch>
2026-03-23 09:10:08 +0100

    [feature] Add not contains in search

--- src/services/yum.rs
+++ src/services/yum.rs
@@ -11,21 +11,42 @@ use crate::utils::{
 
 pub async fn get_restaurant(food_type: &str) -> String {
     let search = food_type.trim().to_lowercase();
-    if food_type.is_empty() {
-        let message = format!("Please say what do you wnat to eat in the command (usage : !yum [filter])\n");
-        return message;
-    }
-    let response = ApiClient::get().await.unwrap();
-    let cafeterias: Vec<Cafeteria> = response.json().await.unwrap();
+    if search.is_empty() {
+        return "Please say what do you want to eat in the command (usage : !yum [filter])\n".to_string();
+    };
+
+    let response = match ApiClient::get().await {
+      Ok(resp) => resp,
+      Err(_) => return "Error, could not reach the restaurant API.".to_string(),
+    };
+
+    let cafeterias: Vec<Cafeteria> = match response.json().await {
+        Ok(data) => data,
+        Err(_) => return "Error, Failed to load menus data".to_string(),
+    };
+
     let mut dishes: Vec<Dish> = filter_menu(cafeterias);
 
-    dishes = dishes.into_iter()
-        .filter(|d| {
-            let name = d.name.to_lowercase();
-            let restaurant = d.restaurant.to_lowercase();
-            let r#type = d.menu_type.to_lowercase();
-            return name.contains(&search) || r#type.contains(&search) || restaurant.contains(&search)
-        })
-        .collect();
+    let (is_exclusion, term) = if search.starts_with('!') {
+        (true, &search[1..])
+    } else {
+        (false, search.as_str())
+    };
+
+    dishes.retain(|d| {
+        let name = d.name.to_lowercase();
+        let restaurant = d.restaurant.to_lowercase();
+        let r#type = d.menu_type.to_lowercase();
+
+        let contains_term = name.contains(term)
+            ||r#type.contains(term)
+            || restaurant.contains(term);
+
+        if is_exclusion {
+            !contains_term
+        } else {
+            contains_term
+        }
+    });
     message(dishes)
 }
