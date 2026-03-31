46857991223cb0da88446c10444e2a63c176afac
antoine <antoine.fabre@epfl.ch>
2026-03-31 08:39:54 +0200

    [fix] Update help command

--- src/services/help.rs
+++ src/services/help.rs
@@ -4,7 +4,7 @@ pub fn get_help() -> String {
     let config = CONFIG.get().expect("Failed to load config");
 
     let help_message = format!("\
-    Available commands: `!menu`, `!yum`, `!oslf`, `!help`
+    Available commands: `!menu`, `!yum`, `!oslf`, `!help`, `!schedule`
     `!schedule` create -d [days] -j [command]    Schedules a command to run automatically at 11:30 AM on the specified day(s). Example: `!schedule create -d mon -j !menu hopper` runs the command every Monday. Use `!schedule -h` for full details.
     `!menu` [restaurant] [filter]       Get a restaurant's menu with an optional filter (e.g., pizza)
     `!yum`  [keyword] [-a <alergen>]    Search all EPFL menus for a food (e.g., pizza), Use `!` to exclude (e.g., !fish), Use `-a` to specify an alergen (e.g., -a gluten).
