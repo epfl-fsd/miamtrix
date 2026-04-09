c1bd4c6102ccfa427c31bf37874e1fc8099610fe
antoine <antoine.fabre@epfl.ch>
2026-04-09 15:49:18 +0200

    [fix] Change logs directory to dynamic location

--- src/utils/logs.rs
+++ src/utils/logs.rs
@@ -1,20 +1,22 @@
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{EnvFilter, Layer, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 use std::fs;
+use std::env;
 
 pub fn init_logging() -> (tracing_appender::non_blocking::WorkerGuard, tracing_appender::non_blocking::WorkerGuard) {
-    let _ = fs::create_dir_all("/logs");
+    let log_dir = env::var("LOG_DIR").unwrap_or_else(|_| "./logs".to_string());
+    let _ = fs::create_dir_all(&log_dir);
 
     let console_layer = fmt::layer()
         .with_target(false);
 
-    let all_file_appender = tracing_appender::rolling::never("logs", "all.log");
+    let all_file_appender = tracing_appender::rolling::never(&log_dir, "all.log");
     let (all_file_writer, guard_all) = tracing_appender::non_blocking(all_file_appender);
     let all_file_layer = fmt::layer()
         .with_writer(all_file_writer)
         .with_ansi(false);
 
-    let err_file_appender = tracing_appender::rolling::never("logs", "errors.log");
+    let err_file_appender = tracing_appender::rolling::never(&log_dir, "errors.log");
     let (err_file_writer, guard_err) = tracing_appender::non_blocking(err_file_appender);
     let err_file_layer = fmt::layer()
         .with_writer(err_file_writer)
